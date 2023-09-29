d0c2ac52e7e21d30a9f4c96c371d797fe3ecdc46
Depard42 <tyomaduobakh@gmail.com>
2023-09-29 03:32:53 +0300

    version 0.12

--- start.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\User\Desktop\Prella\venv\Scripts\activate.bat
-C:\"Program Files"\Python311\python wsgi.py
-pause
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
 from app.app import app, socketio
 
-socketio.run(app, debug="True",host="0.0.0.0",port=8080)
+socketio.run(app, debug="False",host="0.0.0.0",port=8080)
